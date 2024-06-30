6fa84bf1c4445d4eb9c00b0e31fafb35881cd88b
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2024-06-30 16:57:47 +0200

    Einführung Objects

---
+++ script.js
@@ -0,0 +1,13 @@
+let myObject = {
+    'name':'Alex',
+    'age': 35,
+    'job': {
+        'name':'AK',
+        'company':'Bosch'
+    },
+    'good_dude':true,
+};
+
+let objectKey = 'job'
+
+console.log(myObject[objectKey]);
