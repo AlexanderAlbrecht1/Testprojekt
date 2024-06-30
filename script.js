139e685b906a6cca2aca02a84d5b0e531e4fb277
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2024-06-30 18:20:33 +0200

    library

--- script.js
+++ script.js
@@ -1,31 +1,54 @@
-let myObject = [
-    {
-        'name': 'Alex',
-        'good_dude': true,
+let library = {
+    "info": {
+      "name": "Stadtbibliothek",
+      "location": {
+        "city": "Musterstadt",
+        "coordinates": { "lat": 48.1351, "lon": 11.582 }
+      }
     },
-    {
-        'name': 'Sandra',
-        'good_dude': false,
-    },
-    {
-        'name': 'Jenni',
-        'good_dude': true,
-    },
-    {
-        'name': 'Justus',
-        'good_dude': false,
-    },
-    {
-        'name': 'Nick',
-        'good_dude': false,
-    },
-];
-
-console.table(myObject)
-
-console.table(myObject.filter((element) => {return element['good_dude'] == false})
-);
-
-console.table(
-    myObject.filter((element) => {return element['name'] == 'Alex'})
-)
+    "sections": {
+      "fiction": [
+        {
+          "shelf": 1,
+          "book": {
+            "title": "Die Verwandlung",
+            "author": {
+              "name": "Franz Kafka",
+              "born": "1883-07-03",
+              "died": "1924-06-03"
+            },
+            "year": 1915,
+            "status": "ausgeliehen"
+          }
+        },
+        {
+          "shelf": 2,
+          "book": {
+            "title": "1984",
+            "author": {
+              "name": "George Orwell",
+              "born": "1903-06-25",
+              "died": "1950-01-21"
+            },
+            "year": 1949,
+            "status": "verfügbar"
+          }
+        }
+      ],
+      "nonFiction": [
+        {
+          "shelf": 3,
+          "book": {
+            "title": "Eine kurze Geschichte der Zeit",
+            "author": {
+              "name": "Stephen Hawking",
+              "born": "1942-01-08",
+              "died": "2018-03-14"
+            },
+            "year": 1988,
+            "status": "verfügbar"
+          }
+        }
+      ]
+    }
+  }
